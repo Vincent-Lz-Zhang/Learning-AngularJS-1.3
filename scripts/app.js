81ffdf68e4289a176f9f8722120107e66fac69a6
Vincent-Lz-Zhang <aspnetguru@outlook.com>
2017-07-01 00:38:37 +1200

    basic  service

--- scripts/app.js
+++ scripts/app.js
@@ -4,31 +4,61 @@
  */
 
 (function() {
-	'use strict';
+    'use strict';
 
-	angular.module("vinceApp", []);
+    angular.module("vinceApp", []);
 
-	angular.module("vinceApp").controller("vinceCtrl", vinceController);
+    angular.module("vinceApp").controller("vinceCtrl", vinceController);
 
-	vinceController.$inject = ['$scope'];
+    vinceController.$inject = ['$scope', '$http'];
 
-	function vinceController($scope) {
-		$scope.displayDefaultContent = true;
-		$scope.expressedContent = "property defined in $scope can also be accessed in angular expression, we will cover angularjs expression soon";
-		$scope.toggleDefaultContent = function () {
-			$scope.displayDefaultContent = !$scope.displayDefaultContent;
-		};
-		function init() {
-			$scope.displayDefaultContent = false;
-		};
-		// init actions go here
-		init();
-	}
+    function vinceController($scope, $http) {
 
-	// a shortcut is, but it is not recommended, for details, refer to https://github.com/mgechev/angularjs-style-guide#controllers
-	/*
-	angular.module("vinceApp").controller("vinceCtrl", function vinceController($scope) {
-		...
-	});
-	*/
+        $scope.displayDefaultContent = true;
+
+        $scope.toggleDefaultContent = function() {
+            $scope.displayDefaultContent = !$scope.displayDefaultContent;
+        };
+
+        function init() {
+            //$scope.displayDefaultContent = false;
+        };
+
+        $scope.loadData = function() {
+            $http.get("server/productData.json")
+                .success(function(data, status, headers, config) {
+                    $scope.products = data;
+                    console.log($scope.products.length);
+                })
+                .error(function(data, status, headers, config) {
+                    alert('Something goes wrong.');
+                })
+                .finally(function() {
+                    // it is exposed by the object because $http returns a promise
+                });
+        };
+
+        $scope.loadDataWithThen = function() {
+            $http.get("server/productData.json").then(function(response) {
+                console.log("Status: " + response.status);
+                console.log("Type: " + response.headers("content-type"));
+                console.log("Length: " + response.headers("content-length"));
+                $scope.products_then = response.data;
+            });
+        }
+
+        // init actions go here
+        init();
+
+
+    }
+
+
+
+    // a shortcut is, but it is not recommended, for details, refer to https://github.com/mgechev/angularjs-style-guide#controllers
+    /*
+    angular.module("vinceApp").controller("vinceCtrl", function vinceController($scope) {
+    	...
+    });
+    */
 })();
